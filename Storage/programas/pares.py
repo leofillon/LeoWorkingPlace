8c8ded81506a39d3cacd9c5ca0e8eb7126704e24
leofillon <a01027135@itesm.mx>
2018-12-07 18:52:11 -0600

    pares.py mod

--- Storage/programas/pares.py
+++ Storage/programas/pares.py
@@ -4,11 +4,12 @@ N = int(input("ingrese la cantidad de numeros que desea ingresar: "))
 
 for I in range(N):
     numeros.append(input("ingrese un numero: "))
-print("Los numeros ingresados son: ", numeros)
+
+print "Los numeros ingresados son: ", numeros
 numeros.sort()
-print("Los numeros ordenados ascendentemente son: ", numeros)
+print "Los numeros ordenados ascendentemente son: ", numeros
 
 for I in numeros:
     if int(I) % 2 == 0:
         pares.append(I)
-print(pares)
+print "los numeros pares son: ", pares
